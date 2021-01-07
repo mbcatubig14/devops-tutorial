ab0686a3139bff272f017d072bb487083bfe33b5
teknikality-central <mbcatubig14@gmail.com>
2021-01-07 22:27:01 +0000

    pushing to feature branch.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Test Feature branch change now 22:25 </p>
         <a
           className="App-link"
           href="https://reactjs.org"
